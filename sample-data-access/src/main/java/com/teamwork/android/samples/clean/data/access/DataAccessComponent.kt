db57b7fe2a9fb5e12ec9f9326bb34c707a6c6fdc
Marco Salis <fast3r@gmail.com>
2023-02-20 10:14:58 +0100

    Gradle: `data-access` is now a Kotlin-only module

--- sample-data-access/src/main/java/com/teamwork/android/samples/clean/data/access/DataAccessComponent.kt
+++ sample-data-access/src/main/java/com/teamwork/android/samples/clean/data/access/DataAccessComponent.kt
@@ -1,9 +1,5 @@
 package com.teamwork.android.samples.clean.data.access
 
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-
 /**
  * Interface that lists all public 'data access' layer components which need to be exposed to the `business` layer.
  *
@@ -11,8 +7,6 @@ import dagger.hilt.components.SingletonComponent
  * [provision methods](https://dagger.dev/api/2.25.2/dagger/Component.html) which then can be used by injected
  * classes in the business layer.
  */
-@EntryPoint
-@InstallIn(SingletonComponent::class)
 interface DataAccessComponent {
 
     companion object {
